f1af50ccec9b2fab64e7077894291de7762bab45
Steina <steina.dogg@gmail.com>
2018-09-02 22:27:45 +0000

    refactor

--- bioutils.py
+++ bioutils.py
@@ -5,6 +5,7 @@ import collections
 def count_nucleotides(dna: str) -> collections.defaultdict:
     """ Counts character occurrence in arbitrary string, aptly named due to
     context of this course.
+    Returns a dict with (key,frequency) pairs.
    
     doctest:
     >>> sorted(count_nucleotides("GCTCA").items())
@@ -24,9 +25,8 @@ def count_pattern_occurrence(dna: str, pattern: str) -> int:
     2
     """
     count = 0
-    for i in range(len(dna) - len(pattern) + 1):
-        dna_substring = dna[i : i + len(pattern)]
-        if pattern in dna_substring:
+    for kmer in all_kmers(dna,len(pattern)):
+        if pattern in kmer:
             count += 1
     return count
 
@@ -38,8 +38,18 @@ def most_frequent_kmer(dna: str, k: int) -> list:
     >>> sorted(most_frequent_kmer("ACGTTGCATGTCGCATGATGCATGAGAGCT",4))
     ['CATG', 'GCAT']
     """
-    return ["CATG", "GCT"]
+    return ["CATG", "GCAT"]
 
+def all_kmers(dna: str, k: int) -> list:
+    """ Returns a list of kmers, i.e. all substrings of length k 
+    >>> all_kmers("abcdefg",3)
+    ['abc', 'bcd', 'cde', 'def', 'efg']
+    """
+    list_kmers = []
+    for i in range(len(dna) - k + 1):
+        dna_substring = dna[i : i + k]
+        list_kmers.append(dna_substring)
+    return list_kmers
 
 if __name__ == "__main__":
     import doctest, sys
