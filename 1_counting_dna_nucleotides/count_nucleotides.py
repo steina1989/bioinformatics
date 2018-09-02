c50cca39497816cb96aae692df41a66df01e8fb2
Steina <steina.dogg@gmail.com>
2018-09-02 15:13:18 +0000

    wip

--- 1_counting_dna_nucleotides/count_nucleotides.py
+++ 1_counting_dna_nucleotides/count_nucleotides.py
@@ -4,18 +4,26 @@ import collections
 
 
 def count_nucleotides(dna: str) -> collections.defaultdict:
-    """ Counts character occurrence in arbitrary string, aptly named in the
-    context of this course. """
+    """ Counts character occurrence in arbitrary string, aptly named due to
+    context of this course.
+    >>> sorted(count_nucleotides("GCTCA").items())
+    [('A', 1), ('C', 2), ('G', 1), ('T', 1)]
+    """
     occurrence_dict = collections.defaultdict(int)
     for char in dna:
         occurrence_dict[char] += 1
     return occurrence_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    import doctest
+
+    doctest.testmod()
+
     """ Pipe dataset in via terminal.
     The i/o format conforms to Rosalind's excercise provided i/o format.
     """
     data = sys.stdin.readline().strip()
     dna = count_nucleotides(data)
-    print(dna['A'], dna['C'], dna['G'], dna['T'])
+    print(dna["A"], dna["C"], dna["G"], dna["T"])
