eb36e198ccc3fcfd149cfe1c8f4b57526d7c9797
Steina <steina.dogg@gmail.com>
2018-10-11 23:12:22 +0000

    wip

--- motifsearch.py
+++ motifsearch.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 import bioutils as b
 from typing import Iterable
-import random
+import random,sys
 
 
 def greedy_motif_search(dna: list, k: int, pseudo_count: bool = False):
-    pass
+
+    best_motifs = [kmer[0:k] for kmer in dna]
+    t = len(dna)
+    
+    for kmer in b.iter_substr(dna[0],k):
+        motif = [kmer]
+
+        for i in range(1,t):
+            profile = b.profile(motif[0:i],False)
+            motif.append(b.profile_most_probable_kmer(profile,dna[i],k))
+        
+        consensus = b.consensus(motif)
+        if b.score(motif,consensus) < b.score(best_motifs, consensus):
+            best_motifs = motif
+    return best_motifs
+        
+
 
 
 def randomized_motif_search(dna: list, k: int):
@@ -13,6 +29,9 @@ def randomized_motif_search(dna: list, k: int):
 
 
 if __name__ == "__main__":
-    import doctest
 
-    doctest.testmod()
+    with open('motif_test.txt') as f:
+        k = int(f.readline().strip().split(" ")[0])
+        dna = [entry.strip() for entry in f.readlines()]
+    greedy_motif_search(dna, k)
+
