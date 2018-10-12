676ae22c44472859a016e2893686d6cc3c7cbf76
Steina <steina.dogg@gmail.com>
2018-10-12 15:52:13 +0000

    wip

--- motifsearch.py
+++ motifsearch.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
 import bioutils as b
-from typing import Iterable
-import random,sys
+import random, sys
 
 
-def greedy_motif_search(dna: list, k: int, pseudo_count: bool = False):
+def greedy_motif_search(dna: list, k: int, pseudo: bool):
 
     best_motifs = [kmer[0:k] for kmer in dna]
+
     t = len(dna)
-    
-    for kmer in b.iter_substr(dna[0],k):
+
+    for kmer in b.iter_substr(dna[0], k):
         motif = [kmer]
 
-        for i in range(1,t):
-            profile = b.profile(motif[0:i],False)
-            motif.append(b.profile_most_probable_kmer(profile,dna[i],k))
-        
-        consensus = b.consensus(motif)
-        if b.score(motif,consensus) < b.score(best_motifs, consensus):
+        for i in range(1, t):
+            profile = b.profile(motif[0:i], pseudo)
+            most_probable = b.profile_most_probable_kmer(profile, dna[i], k)
+            motif.append(most_probable)
+
+        if b.score(motif) < b.score(best_motifs):
             best_motifs = motif
     return best_motifs
-        
-
-
-
-def randomized_motif_search(dna: list, k: int):
-    pass
 
 
 if __name__ == "__main__":
 
-    with open('motif_test.txt') as f:
+    with open(sys.argv[1]) as f:
         k = int(f.readline().strip().split(" ")[0])
         dna = [entry.strip() for entry in f.readlines()]
-    greedy_motif_search(dna, k)
-
+    res = greedy_motif_search(dna, k, False)
+    print("\n".join(res))
