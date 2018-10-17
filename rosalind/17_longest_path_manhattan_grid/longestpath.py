5150e4cf643fb11a129cd70ab4baef9120461b86
Steina <steina.dogg@gmail.com>
2018-10-17 14:12:20 +0000

    wip

--- rosalind/17_longest_path_manhattan_grid/longestpath.py
+++ rosalind/17_longest_path_manhattan_grid/longestpath.py
@@ -2,9 +2,35 @@
 
 import io, numpy as np,sys
 
-def main(n, m, down, right):
+def main(n, m, d, r):
 
-    x = np.zeros((n,m))
+    x = np.zeros((n+1,m+1))
+
+    for i in range(n+1):
+        for j in range(m+1):
+            down = from_above(x,d,i,j)
+            right = from_left(x,r,i,j)
+
+            x[i,j] = max(down,right)
+    
+    print(x)
+    return x[n,m]
+
+def from_above(x,down,i,j):
+    prev_node = 0
+    upper = 0
+    if i-1 >= 0:
+        prev_node = x[i-1,j]
+        upper = down[i,j]
+    return prev_node + upper
+
+def from_left(x,right,i,j):
+    prev_node = 0
+    rightnumber = 0
+    if j-1 >= 0:
+        prev_node = x[i,j-1]
+        rightnumber = right[i,j]
+    return prev_node + rightnumber
 
 
 def parse_input(file):
@@ -19,4 +45,3 @@ if __name__ == "__main__":
     input_data = parse_input(sys.argv[1])
     main(*input_data)
 
-    
