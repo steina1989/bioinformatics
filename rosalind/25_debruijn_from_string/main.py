f25d1c242035f98575835d70e5c57367e7981937
Steina <steina.dogg@gmail.com>
2018-10-24 13:39:17 +0000

    wip

--- rosalind/25_debruijn_from_string/main.py
+++ rosalind/25_debruijn_from_string/main.py
@@ -3,13 +3,13 @@ import sys
 from bioutils import de_bruijn_from_string
 
 
-def main(dna,k):
-    graph = de_bruijn_from_string(dna,k)
+def main(dna, k):
+    graph = de_bruijn_from_string(dna, k)
     for key, val in sorted(graph.items()):
-        print(key,"->", ",".join(sorted(val)))
+        print(key, "->", ",".join(sorted(val)))
 
 
 if __name__ == "__main__":
     K = int(sys.stdin.readline().strip())
     DNA = sys.stdin.readline().strip()
-    main(DNA,K)
+    main(DNA, K)
