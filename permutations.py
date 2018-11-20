c099a458444fad9aa0830c3e4750bbc6299461fa
Steina <steina.dogg@gmail.com>
2018-11-20 14:38:27 +0000

    wip

--- permutations.py
+++ permutations.py
@@ -3,24 +3,45 @@
 
 class Permutations:
     def __init__(self, input):
-        self.permutation = self.__parse__(input)
+        self.permutation = self.__parse(input)
 
     def greedy_sort(self):
-        self.__reverse__(0, len(self.permutation))
-        return self
+        for i in range(len(self.permutation)):
+            sorted_val = i + 1
+            if self.permutation[i] != sorted_val:
+                index = self.__find_index(sorted_val)
+                self.__reverse(i,index)
+            if self.permutation[i] < 0:
+                self.__reverse(i,i)
+    
+    def __find_index(self, val):
+        for i, item in enumerate(self.permutation):
+            if val == item or val == -item:
+                return i
 
-    def __reverse__(self, a, b):
-        reverse = self.permutation[a:b][::-1]
+    def __reverse(self, a, b):
+        reverse = self.permutation[a:b+1][::-1]
         negate = list(map(lambda x: -x, reverse))
-        self.permutation[a:b] = negate
+        self.permutation[a:b+1] = negate
+        print(self)
 
-    def __parse__(self, input):
-        return list(map(int, input[1:-1].split()))
+    def __parse(self, inp):
+        return list(map(int, inp[1:-1].split()))
 
     def __str__(self):
-        return "(" + " ".join(map(self.__i2str__, self.permutation)) + ")"
+        return "(" + " ".join(map(self.__i2str, self.permutation)) + ")"
 
-    def __i2str__(self, i):
+    def __i2str(self, i):
         if i > 0:
             return "+" + str(i)
         return str(i)
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1]) as f:
+        perm = Permutations(f.readline().strip())
+
+    perm.greedy_sort()
+
+
