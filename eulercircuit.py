28fc616ffb81789858c72e7e2b8cbad64c79bdb0
Steina <steina.dogg@gmail.com>
2018-11-28 09:48:34 +0000

    wip

--- eulercircuit.py
+++ eulercircuit.py
@@ -1,45 +1,79 @@
 #!/usr/bin/env python3
 import collections
 import sys
+import json
 
 
 class EulerCircuit:
     def __init__(self, graph, num_edges):
         self.graph = graph
         self.travelled = []
-        # ERUm am pæla hvort það sé ekki betra að geyma tvenndir af (frá,til) nóðum sem merkja þá leggina.
-        # Hvernig sem það fer, ath að laga is_cycle og unexplored edges dæmið.
         self.num_edges = num_edges
 
     @classmethod
     def from_file(cls, path):
         num_edges = 0
-        graph = collections.defaultdict(list)
+        graph = collections.defaultdict(set)
         with open(path) as f:
             for line in f.readlines():
                 node, out_neighbours = line.strip().split(" -> ")
                 for neighbour in out_neighbours.split(","):
-                    graph[node].append(neighbour)
+                    graph[node].add((neighbour, False))
                     num_edges += 1
         return cls(graph, num_edges)
 
     def cycle(self):
-        # Bútil hring
-        start_node = next(iter(self.graph))
 
         # Make cycle
+        start_node = next(iter(self.graph))
+
         self.traverse(start_node)
 
         while self.there_are_unexplored_edges():
             # Choose a node in self.travelled that has out neighbour that hasnt been used
             # Transform self.travelled so that it starts at this node from above.
-            self.traverse(start_node)
+            
+            # Finna nýjan punkt..
+            node = self.find_new_start()
+            self.fix_travelled(node)
+
+            untravelled_node = self.find_untravelled(self.graph[node])[0]
+            # Laga self.travelled svo það fari sama hring, nema frá nýja punktinum.
+            self.traverse(untravelled_node)
 
     def traverse(self, node):
-        while not self.cycle():
-            neighbours = self.graph[node]
-            self.travelled.append(neighbours[0])
-            node = neighbours[0]
+
+        while not self.is_cycle():
+            self.travelled.append(node)
+            set_neighbours = self.graph[node]
+            neighbour = self.find_untravelled(set_neighbours)
+            if not neighbour:
+                continue
+            self.mark_travelled(node,neighbour)
+            node = neighbour[0]
+
+    def fix_travelled(self,node):
+        out = self.travelled
+
+        out = out[0:-1]
+        index = out.index(node)
+        self.travelled = out[index:] + out[0:index] + [node]
+
+    def find_new_start(self):
+        for key in (set(self.travelled)):
+            if self.find_untravelled(self.graph[key]):
+                return key
+
+
+    def find_untravelled(self, set_neighbours):
+        for neighbour in set_neighbours:
+            if not neighbour[1]:
+                return neighbour
+        return None
+
+    def mark_travelled(self, node, neighbour):
+        self.graph[node].remove((neighbour[0], False))
+        self.graph[node].add((neighbour[0], True))
 
     def there_are_unexplored_edges(self):
         return self.num_edges > len(self.travelled) - 1
@@ -48,12 +82,22 @@ class EulerCircuit:
         return "->".join(self.travelled)
 
     def is_cycle(self):
+        if len(self.travelled) <= 1:
+            return False
         return self.travelled[0] == self.travelled[-1]
+
+    def debug_print(self):
+        for x, y in self.graph.items():
+            print(x, y)
 
 
 if __name__ == "__main__":
     import sys
 
-    eu = EulerCircuit.from_file(sys.argv[1])
-
-    print(eu.cycle())
+    eu = None
+    if len(sys.argv) == 1:
+        eu = EulerCircuit.from_file('rosalind/27_eulerian_cycle/test.txt')
+    else:
+        eu = EulerCircuit.from_file(sys.argv[1])
+    eu.cycle()
+    print(eu)
