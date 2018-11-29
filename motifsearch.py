0d6df0d9eadb2f0fd88e6928eaca45cf660e14ec
Steina <steina.dogg@gmail.com>
2018-11-29 14:18:30 +0000

    Wip

--- motifsearch.py
+++ motifsearch.py
@@ -21,40 +21,44 @@ def greedy_motif_search(dna: list, k: int, pseudo: bool):
             best_motifs = motif
     return best_motifs
 
-def randomized_motif_search(dna,k):
-    best_motifs = _randomized_motif_search(dna,k)
+
+def randomized_motif_search(dna, k):
+    best_motifs = _randomized_motif_search(dna, k)
     for _ in range(1001):
-        motifs = _randomized_motif_search(dna,k)
+        motifs = _randomized_motif_search(dna, k)
         if b.score(motifs) < b.score(best_motifs):
             best_motifs = motifs
     return best_motifs
+
 
 def _randomized_motif_search(dna, k):
 
     # Randomly select kmers Motifs in each string
     motifs = []
     for string in dna:
-        r = random.randint(0,len(string)-k)
-        motifs.append(string[r:r+k])
+        r = random.randint(0, len(string) - k)
+        motifs.append(string[r : r + k])
     best_motifs = motifs
 
     while True:
         profile = b.profile(motifs, True)
-        motifs = _motifs(profile, dna,k)
+        motifs = _motifs(profile, dna, k)
         if b.score(motifs) < b.score(best_motifs):
             best_motifs = motifs
         else:
             return best_motifs
 
 
-def _motifs(profile, dna,k):
+def _motifs(profile, dna, k):
     motifs = []
     for sequence in dna:
-        motifs.append(b.profile_most_probable_kmer(profile, sequence,k))
+        motifs.append(b.profile_most_probable_kmer(profile, sequence, k))
     return motifs
+
 
 if __name__ == "__main__":
     import sys
+
     with open(sys.argv[1]) as f:
         k = int(f.readline().strip().split(" ")[0])
         dna = [entry.strip() for entry in f.readlines()]
